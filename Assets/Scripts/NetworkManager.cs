647e6e02cc9b2550bcdd9bc87f1f0297164cc6b4
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-07 13:59:14 +0100

    Implemented method to activate and deactivate relevant UI panels

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,19 +6,34 @@ using Photon.Pun;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    [Header("Login UI")]
+    [Header("Login UI Panel")]
     public InputField playerNameInput;
-    
+    public GameObject loginPanel;
+
     [Header("Connection Status")]
     public Text connectionStatusText;
 
+    [Header("Game Options UI Panel")]
+    public GameObject gameOptionsPanel;
+
+    [Header("Create Room UI Panel")]
+    public GameObject createRoomPanel;
+
+    [Header("Inside Room UI Panel")]
+    public GameObject insideRoomPanel;
+
+    [Header("Room List UI Panel")]
+    public GameObject roomListPanel;
+
+    [Header("Join Random Room UI Panel")]
+    public GameObject joinRandomRoomPanel;
 
     #region Unity Methods
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ActivatePanel(loginPanel.name);
     }
 
     // Update is called once per frame
@@ -26,6 +41,26 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         // Display connection status
         connectionStatusText.text = "Connection status : " + PhotonNetwork.NetworkClientState;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+
+    #endregion
+
+    #region Public Methods
+
+    // Activate only relevant panel and deactivate the rest 
+    public void ActivatePanel(string panelToActivate)
+    {
+        loginPanel.SetActive(panelToActivate.Equals(loginPanel.name));
+        gameOptionsPanel.SetActive(panelToActivate.Equals(gameOptionsPanel.name));
+        createRoomPanel.SetActive(panelToActivate.Equals(createRoomPanel.name));
+        roomListPanel.SetActive(panelToActivate.Equals(roomListPanel.name));
+        insideRoomPanel.SetActive(panelToActivate.Equals(insideRoomPanel.name));
+        joinRandomRoomPanel.SetActive(panelToActivate.Equals(joinRandomRoomPanel.name));
     }
 
     #endregion
@@ -62,6 +97,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon server");
+        // Activating game options panel after successfull connection to the Photon server
+        ActivatePanel(gameOptionsPanel.name);
     }
 
     #endregion
