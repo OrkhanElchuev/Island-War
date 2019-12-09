a9a2889362916a6bd9a0a27fbc8c234c57acc9c0
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-09 03:49:46 +0100

    Shooting mechanism implemented, temporary aim added

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Camera FPS_Camera;
+
+    // Shooting via sending invisible Rays
+    public void Attack()
+    {
+        RaycastHit hit;
+        // Send a ray to the middle of the camera view(screen)
+        Ray ray = FPS_Camera.ViewportPointToRay(new Vector3(0.5f, 0, 5f));
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
