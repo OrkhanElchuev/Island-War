bb6b7d3d476295d540c9575e727e03fe554e4c7d
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-09 01:16:40 +0100

    Player movement via joystick implemented

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public Joystick joystick;
+    private RigidbodyFirstPersonController rbController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    private void FixedUpdate()
+    {
+        rbController.joystickInputAxis.x = joystick.Horizontal;
+        rbController.joystickInputAxis.y = joystick.Vertical;
+    }
+}
