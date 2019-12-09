f06e3a76a55f2bb8ff18421c7a8ae11562662236
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-09 04:03:12 +0100

    Particle System from Standard Asset package imported, hitting effect added

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Camera FPS_Camera;
+    public GameObject hitEffectPrefab;
 
     // Shooting via sending invisible Rays
     public void Attack()
     {
         RaycastHit hit;
         // Send a ray to the middle of the camera view(screen)
-        Ray ray = FPS_Camera.ViewportPointToRay(new Vector3(0.5f, 0, 5f));
+        Ray ray = FPS_Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
 
         if (Physics.Raycast(ray, out hit, 100))
         {
             Debug.Log(hit.collider.gameObject.name);
+            // Run hitting effect particle
+            GameObject hitEffectGameObject = Instantiate(hitEffectPrefab, hit.point, Quaternion.identity);
+            Destroy(hitEffectGameObject, 0.5f);
         }
     }
 }
