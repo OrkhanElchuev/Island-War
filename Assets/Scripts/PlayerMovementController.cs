f47820095fadf6aed11d4daa551ce9f28dfb9983
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-12-09 02:45:55 +0100

    Running Blend Tree player animation implemented

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,6 +7,8 @@ public class PlayerMovementController : MonoBehaviour
 {
     public Joystick joystick;
     public FixedTouchField fixedTouchField;
+
+    private Animator animator;
     private RigidbodyFirstPersonController rbController;
 
 
@@ -14,6 +16,7 @@ public class PlayerMovementController : MonoBehaviour
     void Start()
     {
         rbController = GetComponent<RigidbodyFirstPersonController>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -21,5 +24,28 @@ public class PlayerMovementController : MonoBehaviour
         rbController.joystickInputAxis.x = joystick.Horizontal;
         rbController.joystickInputAxis.y = joystick.Vertical;
         rbController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;
+
+        // Set up animation speed according to the joystick (player movement speed)
+        animator.SetFloat("Horizontal", joystick.Horizontal);
+        animator.SetFloat("Vertical", joystick.Vertical);
+
+        RunningAndWalking();
+    }
+
+    // Transition between running and walking states
+    private void RunningAndWalking()
+    {
+        // Joystick's max forward values is 1 max backward is -1
+        // Make a transition to running state at 0.9 position of joystick
+        if (Mathf.Abs(joystick.Horizontal) > 0.9f || Mathf.Abs(joystick.Vertical) > 0.9f)
+        {
+            rbController.movementSettings.ForwardSpeed = 10;
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            rbController.movementSettings.ForwardSpeed = 6;
+            animator.SetBool("IsRunning", false);
+        }
     }
 }
